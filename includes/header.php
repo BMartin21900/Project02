4d1cd603270bd1a38825a30de53391218f818aa3
gromanaz <gromanaz@users.noreply.github.com>
2021-03-25 14:20:33 +0100

    prepared possible header php include

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header>
+  <h1>Dulio Foundation</h1>
+
+  <nav class="menu">
+    <ul>
+      <li><<a href="index.html">Home</a></li>
+      <li><a href = "copenhagen.html">Copenhagen</a></li>
+      <li><a href = "fuzhou.html">Fuzhou</a></li>
+      <li><a href = "nairobi.html">Nairobi</a></li>
+      <li><a href = "oberursel.html">Oberursel</a></li>
+    </ul>
+  </nav>
+
+</header>
+
+
+<!--add this in html:
+<?php include "includes/header.php"; ?>
+-->
