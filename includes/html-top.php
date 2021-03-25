2c2579698d3620dd1615cbf2520706655f65878f
gromanaz <gromanaz@users.noreply.github.com>
2021-03-25 14:30:29 +0100

    added possible php include, revised head

---
+++ includes/html-top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DMS 290 - Project 2 - Team Gotham</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<!--add this in html:
+<?php include "includes/html-top.php"; ?>
+-->
