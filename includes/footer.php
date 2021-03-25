f5138bd0d37b2d35929c05031680fbde595e9dd4
gromanaz <gromanaz@users.noreply.github.com>
2021-03-25 17:00:34 +0100

    prepared potential php footer

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div>Web Design by Qianya Lin, Bradley Martin, Gina Romanazzi</div>
+  <div>DMS 290 - Front-End Web Design</div>
+</footer>
+
+<!--add this in html:
+<?php include "includes/footer.php"; ?>
+-->
