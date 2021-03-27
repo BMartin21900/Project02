a1bceb450716b59fd25bddd7b53423a9e7033cf3
gromanaz <gromanaz@users.noreply.github.com>
2021-03-27 22:36:53 +0100

    installed fonts in html

--- includes/html-top.php
+++ includes/html-top.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>DMS 290 - Project 2 - Team Gotham</title>
   <link rel="stylesheet" href="css/styles.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Abel&family=Roboto&display=swap" rel="stylesheet">
 </head>
 
 <!--add this in html:
